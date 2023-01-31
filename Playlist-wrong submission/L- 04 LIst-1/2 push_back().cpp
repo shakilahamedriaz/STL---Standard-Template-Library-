7088612a0be7da145aca8d29bf5d7efdd056ddbe
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-02-01 00:02:50 +0600

    done

---
+++ Playlist-wrong submission/L- 04 LIst-1/2 push_back().cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int>li;
+
+    li.push_back(1);
+    li.push_back(2);
+    li.push_back(3);
+
+
+    list<int>::iterator it;
+
+
+
+
+    cout<< "using auto iterator"<<endl;
+
+    for(auto it: li)
+    {
+
+        cout<<it<<endl;
+    }
+    cout<<endl;
+
+    return 0;
+}
+
