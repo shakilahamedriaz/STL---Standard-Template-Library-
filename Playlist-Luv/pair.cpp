68d7b3c04b4a4f9b16f763e04f3ffe8ec92b7f90
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-02-15 01:45:35 +0600

    aaaaaa

---
+++ Playlist-Luv/pair.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+pair<int,string>p;
+
+//p=make_pair(2, "abc");
+p= {2,"abc"};
+
+cout<<p.first<< " "<<p.second<<endl;
+
+
+
+
+    return 0;
+}
