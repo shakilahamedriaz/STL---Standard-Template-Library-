c4e67180f4f9deccaa5c51376a5f85c462db87b2
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-02-17 00:03:14 +0600

    gcd , lcm euclid's algorithm

---
+++ Number Theory/3.gdc_Euclidean Algorithm recursive.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if(b==0) return a;
+    return gcd(b, a%b);
+     
+}
+
+int main()
+{
+
+    cout<< "All gcd ans: "<<endl;
+     cout<<gcd(12, 4)<<endl;
+     cout<<gcd(18, 12)<<endl;
+     cout<<gcd(12, 18)<<endl;
+
+    cout<< "Lcm of 12, 18 : ";
+    cout<< 12*18/gcd(12,18)<<endl;
+
+    return 0;
+}
